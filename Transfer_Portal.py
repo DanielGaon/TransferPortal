60a8970257e1abf4053bd6489c47d9785b2365d7
Daniel Gaon <dgaon48@gmail.com>
2025-07-31 23:21:41 +0300

    Flask updates

--- Transfer_Portal.py
+++ Transfer_Portal.py
@@ -6,12 +6,12 @@ import subprocess
 app = Flask("Transfer")
 
 UPLOAD_FOLDER = 'uploads'
-SCANNED_FOLDER = 'scanned'
+#SCANNED_FOLDER = 'scanned' - for futre use to scan the files
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'zip', 'xlsx', 'docx'}
 
 # Ensures directories exist
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-os.makedirs(SCANNED_FOLDER, exist_ok=True)
+#os.makedirs(SCANNED_FOLDER, exist_ok=True) for futre use to scan the files 
 
 # Set config
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -27,7 +27,7 @@ def favicon():
 #Main page
 @app.route("/")
 def main_page():
-    result = {'msg':"Welcome to Altshare-File Transfer Portal"}
+    result = {'messege':"Welcome to Altshare-File Transfer Portal"}
     return result
 
 #This function checks whether a given filename is allowed based on its file extension 
@@ -35,7 +35,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
+#Uploads a file
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
@@ -54,13 +54,32 @@ def upload_file():
 
     return jsonify({'error': 'File type not allowed'}), 400
 
-
-    
-
-#Returns a list of filenames in that folder that have already been scanned and uploaded successfully
+#Gets the list of uploaded files
 @app.route('/files', methods=['GET'])
 def list_files():
-    files = os.listdir(SCANNED_FOLDER)
-    return jsonify({'scanned_files': files})
+    files = os.listdir(UPLOAD_FOLDER)
+    return jsonify({'uploaded_files': files})
+
+#Deletes a specific file
+@app.route('/files/<name>', methods=['DELETE'])
+def delete_file(name):
+    filepath = os.path.join(UPLOAD_FOLDER, secure_filename(name))
+    
+    if os.path.exists(filepath):
+        os.remove(filepath)
+        return jsonify({'message': f'{name} deleted successfully'}), 200
+    else:
+        return jsonify({'error': 'File not found'}), 404
+    
+#Opens the file    
+@app.route('/files/<name>', methods=['GET'])
+def get_file(name):
+    safe_name = secure_filename(name)
+    filepath = os.path.join(UPLOAD_FOLDER, safe_name)
+
+    if os.path.exists(filepath):
+        return send_from_directory(UPLOAD_FOLDER, safe_name, as_attachment=False)
+    else:
+        return jsonify({'error': 'File not found'}), 404
 
 app.run('0.0.0.0',8080)
